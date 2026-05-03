b7bd3113cb774cfb8a2b07212974cf097ccc74e6
M. Berke Dur <mdur@trinity.edu>
2026-05-03 16:53:41 -0500

    convolutional denoise first push

--- DenoisingAutoencoder/conv_denoise_MNIST.py
+++ DenoisingAutoencoder/conv_denoise_MNIST.py
@@ -2,9 +2,60 @@ import matplotlib
 matplotlib.use("TkAgg")
 from keras.datasets import mnist
 from keras.models import Sequential
-from keras.layers import Dense, Conv2D
+from keras.layers import Dense, Conv2D, Flatten, Reshape
 from matplotlib import pyplot as plt
 import numpy as np
 import random
 
 ## ** YOUR CODE HERE **
+
+(xRef, yRef), _ = mnist.load_data()
+
+xTrain = xRef[..., np.newaxis] / 255.0        # (60000, 28, 28, 1)
+noise = np.random.normal(0, 0.4, xTrain.shape)
+noisedSet = xTrain + noise
+
+model = Sequential()
+# Encoder
+model.add(Conv2D(32, 3, activation='relu', padding='same', input_shape=(28,28,1)))
+model.add(Conv2D(32, 3, activation='relu', padding='same'))
+# Latent Representation
+model.add(Flatten())
+model.add(Dense(512, activation='relu'))
+# Decoder
+model.add(Dense(28*28*32, activation='relu'))
+model.add(Reshape((28, 28, 32)))
+model.add(Conv2D(32, 3, activation='relu', padding='same'))
+model.add(Conv2D(1, 3, activation='sigmoid', padding='same'))
+
+model.compile(optimizer='adam', loss='mse')
+model.fit(noisedSet, xTrain, batch_size=32, epochs=10)
+
+denoised = model.predict(noisedSet)
+
+imgs = denoised.reshape(xRef.shape[0], xRef.shape[1], xRef.shape[2])
+originals = xTrain.reshape(xRef.shape[0], xRef.shape[1], xRef.shape[2])
+noisy = noisedSet.reshape(xRef.shape[0], xRef.shape[1], xRef.shape[2])
+
+fig, axes = plt.subplots(3, 10, figsize=(15, 6))
+row_titles = ["Original Images", "Input with Noise Added", "Denoised Output"]
+for ax, title in zip(axes[:, 0], row_titles):
+    ax.set_ylabel(title, fontsize=12)
+
+digs = set()
+i = 0
+while len(digs) < 10:
+    label = yRef[i]
+    if label not in digs:
+        axes[0, label].imshow(originals[i], cmap='gray')
+        axes[0, label].set_title(str(label))
+        axes[0, label].axis('off')
+        axes[1, label].imshow(noisy[i], cmap='gray')
+        axes[1, label].axis('off')
+        axes[2, label].imshow(imgs[i], cmap='gray')
+        axes[2, label].axis('off')
+        digs.add(label)
+    i += 1
+
+plt.tight_layout()
+plt.show()
