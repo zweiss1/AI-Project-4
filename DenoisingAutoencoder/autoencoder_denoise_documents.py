17f976a3b7deba160f43da0d99099f74f4d3eb79
Zach Weiss <zweiss1@trinity.edu>
2026-05-05 00:44:46 -0500

    Can train a model on a subset of documents, pushing so I can try using the HPC cluster to train on a larger dataset faster

--- DenoisingAutoencoder/autoencoder_denoise_documents.py
+++ DenoisingAutoencoder/autoencoder_denoise_documents.py
@@ -1,1 +1,80 @@
  # ** YOUR STUFF HERE **
+import matplotlib
+matplotlib.use("TkAgg")
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Conv2D, Flatten, Reshape, UpSampling2D
+from keras.preprocessing.image import ImageDataGenerator
+from matplotlib import pyplot as plt
+import numpy as np
+import random
+import tensorflow as tf
+from PIL import Image
+import os
+
+
+#Load the noisy documents dataset
+def load_noisy_documents():
+    X, Y = [], []
+    count = 0
+    for fname in sorted(os.listdir("Noisy_Documents/noisy/540x420/")):
+        # load feature image
+        x = Image.open(os.path.join("Noisy_Documents/noisy/540x420", fname)).convert("L")
+        x = np.array(x).astype("float32") / 255.0
+
+        # load label image
+        y = Image.open(os.path.join("Noisy_Documents/clean/540x420", fname)).convert("L")
+        y = np.array(y).astype("float32") / 255.0
+
+        # add channel dimension
+        X.append(x[..., None])
+        Y.append(y[..., None])
+        count += 1
+        #Shrink the dataset for testing
+        if count > 70:
+            break
+
+    X = np.array(X)
+    Y = np.array(Y)
+    return X, Y
+
+def build_autoencoder():
+    model = Sequential()
+    # Encoder
+    model.add(Conv2D(32, 3, activation='relu', padding='same', input_shape=(420, 540, 1)))
+    model.add(Conv2D(64, 3, strides=2, activation='relu', padding='same'))
+    # Latent Representation (really just a bottleneck)
+    model.add(Conv2D(128, 3, strides=2, activation='relu', padding='same'))
+    # Decoder
+    model.add(UpSampling2D())
+    model.add(Conv2D(64, 3, activation='relu', padding='same'))
+    model.add(UpSampling2D())
+    model.add(Conv2D(32, 3, activation='relu', padding='same'))
+    #Output layer
+    model.add(Conv2D(1, 3, activation='sigmoid', padding='same'))
+
+    model.compile(optimizer='adam', loss='mse')
+    return model
+
+def train_autoencoder(model, noisy_source, clean_source, epochs=10):
+    model.fit(x=noisy_source, y=clean_source, epochs=10, steps_per_epoch=12, batch_size=12)
+
+
+def main():
+    epochs = input("number of epochs (default 10) ")
+    if epochs == "":
+        epochs = 10
+    else:
+        epochs = int(epochs)
+    model = build_autoencoder()
+    x, y = load_noisy_documents()
+    model.fit(
+        x=x,
+        y=y,
+        epochs=epochs,
+        steps_per_epoch=12
+    )
+    model.save("autoencoder_540x420_70imgs_5epochs_v1.keras")
+
+    
+main()
--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # AI-Project-4
 Hi berke :)
 Hi Milo!
+
+
+Zach's Notes:
+
+The images in the office dataset come in two different sizes: 540x258 and 540x420. There are 72 of the former and 144 of the latter, so I'm just going to ignore the 540x258.
+However, if we wanted to later on, maybe we could add blank pixels to stretch them out to be 540x420 and see if the model gets better.
